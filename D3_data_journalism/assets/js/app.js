c986d070e70f7bf7e840ac576789b865fab88768
Robin Christoffersen <robin.christoffersen@gmail.com>
2021-07-03 12:14:25 -0700

    Working on load

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -11,17 +11,19 @@ var margin = {
 // Define scatter width and height (svg area - margins)
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
-// Create an SVG wrapper, append a SVG group that holds the chart, 
+// Create an SVG wrapper, append SVG that holds the chart
 var svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
-// Append the SVG group and shift the group by left and top margins
+// Append the scatter group and shift the group by left and top margins
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Initial Xaxis
 var chosenXAxis = "poverty";
+// Initial Yaxis
+var chosenYAxis = "healthcare";
 
 //----------------------------------------
 // function used for updating x-scale var upon click on axis label
@@ -34,10 +36,8 @@ function xScale(censusData, chosenXAxis) {
     .range([0, width]);
 
   return xLinearScale;
-
 }
 //----------------------------------------
-
 // function used for updating xAxis var upon click on axis label
 function renderAxes(newXScale, xAxis) {
   var bottomAxis = d3.axisBottom(newXScale);
@@ -53,9 +53,6 @@ function renderAxes(newXScale, xAxis) {
 d3.csv("assets/data/data.csv").then((censusData, err) => {
   if (err) throw err;
 
-  // Print the census data
-  console.log(censusData);
-
   // Parse data - Cast as numbers
   censusData.forEach(data => {
       data.healthcare = +data.healthcare;
@@ -63,6 +60,7 @@ d3.csv("assets/data/data.csv").then((censusData, err) => {
       data.povertyMoe = +data.povertyMoe; 
       data.age = +data.age;   
   });
+  // Print the census data
   console.log(censusData);
 
   // xLinearScale function above csv import
@@ -107,14 +105,14 @@ d3.csv("assets/data/data.csv").then((censusData, err) => {
     .attr("y", 20)
     .attr("value", "poverty") // value to grab for event listener
     .classed("active", true)
-    .text("Poverty");
+    .text("In Poverty (%)");
 
   var ageLabel = labelsGroup.append("text")
     .attr("x", 0)
     .attr("y", 40)
     .attr("value", "age") // value to grab for event listener
     .classed("inactive", true)
-    .text("Age");
+    .text("Age (Median)");
 
   // append y axis
   chartGroup.append("text")
@@ -123,7 +121,7 @@ d3.csv("assets/data/data.csv").then((censusData, err) => {
     .attr("x", 0 - (height / 2))
     .attr("dy", "1em")
     .classed("axis-text", true)
-    .text("Healthcare");
+    .text("Lacks Healthcare (%)");
 
   // updateToolTip function above csv import
   var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
